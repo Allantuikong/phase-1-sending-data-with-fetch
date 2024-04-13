1f10c3150cd859e9cf64e587b4a9f08f95bef5cf
Allan Kipkorir <allantuix@gmail.com>
2024-04-13 11:47:42 +0300

    used fetch requests to POST data to a server

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const userData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      const newId = object.id;
+      const idElement = document.createElement("p");
+      idElement.textContent = `New ID: ${newId}`;
+      document.body.appendChild(idElement);
+    })
+    .catch(function (error) {
+      const errorElement = document.createElement("p");
+      errorElement.textContent = ` Error: ${error.message}`;
+      document.body.appendChild(errorElement);
+    });
+}
